0192deef269d473d120438125e028918c4f8c5db
altinahodaj <76071528+altinahodaj@users.noreply.github.com>
2021-04-26 03:00:56 +0200

    Administratori Crud created

---
+++ Backend/Backend/Models/Administratori.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Administratori
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Birthday { get; set; }
+        public string ParentName { get; set; }
+        public string Hometown { get; set; }
+    }
+}
