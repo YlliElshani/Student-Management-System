c74d26c9d78f15db053a239adaa63210323c7b09
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-05-17 21:55:29 +0200

    deleted files

--- SMS.sql
+++ SMS.sql
@@ -1,43 +1,74 @@
+
 create database smsDB
 use smsDB
 
-create table Student (
-	StudentID int identity(1,1) primary key,
+create table Useri(
+	UserID int identity(1,1) primary key,
 	FirstName varchar (50),
 	LastName varchar (50),
 	Gender varchar (7),
 	Age int,
-	Address varchar (255),
 	PhoneNumber varchar(100),
-	Email varchar (255),
-	Birthday varchar (70),
-	ParentName varchar (50),
-	Hometown varchar (255)
+	Email varchar (255)
 )
 
-insert into Student values ('Hysnije', 'Zllanoga', 'F', 20, 'Mizair Isma', '049604201', 'hysnijee.zllanoga@gmail.com', '03-06-2001', 'Lulzim', 'Rahovec')
- select * from Student
-
- create table Administratori (
+create table Administratori (
 	AdministratoriID int identity(1,1) primary key,
-	FirstName varchar (50),
-	LastName varchar (50),
-	Gender varchar (7),
-	Age int,
 	Address varchar (255),
-	PhoneNumber varchar(100),
-	Email varchar (255),
+	Birthday varchar (70),
+	Hometown varchar (255),
+	Useri int,
+	foreign key (Useri) references Useri(UserID)
+)
+
+create table Student(
+	StudentID int identity(1,1) primary key,
 	Birthday varchar (70),
 	ParentName varchar (50),
-	Hometown varchar (255)
- )
- insert into Administratori values('Alban', 'Rexhepi', 'M', 36, 'Muharrem Fejza', '049564428', 'alban.rexhepi@gmail.com', '04-04-1985', 'Bashkim', 'Prishtine')
- select * from Administratori
+	Hometown varchar (255),
+	Address varchar(200),
+	Useri int,
+	foreign key (Useri) references Useri(UserID)
+)
 
- create table Nota(
+create table Prindi(
+	PrindiID int identity(1,1) primary key,
+	Useri int,
+	foreign key (Useri) references Useri(UserID)
+)
+
+create table Nota(
 	NotaID int identity(1,1) primary key,
 	Grade varchar (10)
- )
+)
 
- insert into Nota values ('A')
- select * from Nota
+create table Profesori(
+	ProfesoriID int identity(1,1) primary key,
+	GradaAkademike varchar(255),
+	Studimi int,
+	Useri int,
+	Foreign key (Studimi) references Studimet(StudimiID),
+	foreign key (Useri) references Useri(UserID)
+)
+
+create table Studimet(
+	StudimiID int identity(1,1) primary key,
+	Fillor varchar(255),
+	IMesem varchar(255),
+	Universitar varchar(255)
+)
+
+create table Lenda(
+	LendaID int identity(1,1) primary key,
+	Lenda varchar(255),
+	Profesori int,
+	foreign key (Profesori) references Profesori(ProfesoriID)
+)
+
+create table Vleresimi(
+	VleresimiID int identity(1,1) primary key,
+	Lenda int,
+	Nota int,
+	foreign key (Lenda) references 
+)
+
