9cb7e43bc7d31a216f6667bd7a0e08d39e068cb7
Hysnijee <hysnijee.zllanoga@gmail.com>
2021-05-18 15:26:17 +0200

    Nota controller done

---
+++ Backend/Backend/Models/Nota.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public class Nota
+    {
+        public int NotaID { get; set; }
+        public string Grade { get; set; }
+    }
+}
