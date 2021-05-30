eedd630db0badcadba11a87c41ef8d1acf129a06
ermira mustafa <mustafaermira@gmail.com>
2021-05-30 22:12:06 +0200

    Merge Conflict --fixed

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -12,11 +12,9 @@ namespace Persistence
         }
 
         public DbSet <User> Users {get; set;}
-<<<<<<< HEAD
+
         public DbSet <Lenda> Lendet {get; set;}
-=======
         public DbSet<Admin> Admins{get; set;}
->>>>>>> 64905f49871440fdbde8d8bad68cb8efe5daa69f
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
